cead72e3bbb3bf7c858bb7e46b234bc813332d76
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-10-20 14:02:49 -0700

    add logic part to display java and c# parts

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,19 @@ $(document).ready(function() {
     var question4 = $("#android").val();
     var question5 = $("#windows").val();
 
+    $(".suggestions").hide();
 
     if (question1==="front") {
       alert("We are so sorry but now we don't run any front-end language classes. Wait for incoming schedule! ")
-    } else if (question2==="small") {
+    } else if (question2==="small" && question3==="2" && question4==="2") {
         $("#ruby").show();
-    // } else if (question2==="enterprise" || question3==="1" || question4==="1") {
-    //     $("java").show();
-    // } else if (question2==="large" || question5==="1") {
-    //     $("#c").show();
+    } else if (question2==="enterprise" && question3==="1" && question4==="1") {
+        $("#java").show();
+    } else if (question2==="large" && question5==="1") {
+        $("#c").show();
+    } else {
+        $("#ruby, #java, #c").show()
+        alert("In your case each language is a good choice");
     }
 
     event.preventDefault();
